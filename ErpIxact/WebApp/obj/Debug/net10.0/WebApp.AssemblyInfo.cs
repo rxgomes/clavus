6188fa62b7769cc133f49d9bead73830de2282a1
Ricardo Gomes <rx.gomes@terra.com.br>
2026-03-26 20:08:42 -0300

    gitignore

--- ErpIxact/WebApp/obj/Debug/net10.0/WebApp.AssemblyInfo.cs
+++ ErpIxact/WebApp/obj/Debug/net10.0/WebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8853c4284501354043667ad0b36a7e6489d698b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60bce94c02f6c21b5098984430f3ce205c3ee59a")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
